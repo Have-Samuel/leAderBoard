12e88c7cccb1040f774e251af90a408b0a9a88c3
Have Samuel <samiehave9@gmail.com>
2022-04-07 01:59:23 +0300

    Added a modules files with some files in it

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import _ from 'lodash';
+/* eslint-disable import/extensions */
 import './style.css';
+import formButton from './modules/scoreBtn.js';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
+formButton();
