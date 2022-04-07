374060fbe8c4f85ee58442417a5278c9bb4e9a30
Have Samuel <samiehave9@gmail.com>
2022-04-07 19:58:57 +0300

    Added the function that communicetes with the API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/extensions */
 import './style.css';
 import formButton from './modules/scoreBtn.js';
+import { createGame } from './modules/addScore.js';
 
+createGame();
 formButton();
--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -1,6 +1,52 @@
+
 const scoreHTML = document.querySelector('#scoreList');
 
-const addScore = (name, value) => {
+export const createGame = async () => {
+  let gameId = null;
+  try {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'HaveGame',
+      }),
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    if (response.ok) {
+      // get game ID
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    return error.message;
+  }
+  return gameId;
+};
+
+// Create a game
+// to get an ID, save that ID
+// create a URL endpoint with that ID
+const createScore = async (user, score) => {
+  try {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LUaUfqXVzn1hoUXCyr49/scores', {
+      method: 'POST',
+      body: { User: user, Score: score },
+      headers: {
+        'Content-type': 'application/json; charaset=UTF-8',
+      },
+    });
+    if (response.ok) {
+      // get game ID
+      game.scores.push({ user, score });
+    }
+  } catch (error) {
+    return error.massage;
+  }
+  return null || { user, score };
+};
+
+const addScore = async (name, value) => {
   const scoreElement = document.createElement('li');
 
   scoreElement.innerHTML = `${name} : ${value}`;
